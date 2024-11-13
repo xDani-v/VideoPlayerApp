dd63f1011273c52647404f38a43a0d6c70ceed8b
xDani <villanodaniel8@gmail.com>
2024-11-13 15:24:34 -0500

    add capacitor

---
+++ src/app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.videoApp.app',
+  appName: 'appVideo',
+  webDir: 'www'
+};
+
+export default config;
